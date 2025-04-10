c8a3326d0d85696d87e6818914fdb3c4841b909b
Rubankumar Velusamy <83352376+Rubankumar-13@users.noreply.github.com>
2025-04-11 02:14:51 +0530

    rm : lombok

--- src/main/java/com/kubernetes/demo/K8SDemoApplication.java
+++ src/main/java/com/kubernetes/demo/K8SDemoApplication.java
@@ -1,8 +1,8 @@
 package com.kubernetes.demo;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
+// import lombok.Builder;
+// import lombok.Data;
+// import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
@@ -52,9 +52,24 @@ class HelloResource {
     }
 }
 
-@Builder
-@Data
+// @Builder
+// @Data
 class Application {
+
     private String name;
     private String version;
+
+    public String getName(){
+        return name;
+    }
+    public String getVersion(){
+        return version;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setVersion(String version){
+        this.version = version;
+    }
 }
